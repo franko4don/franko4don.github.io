90e7add7b0049eb9c10b2d839091dc06e14695ad
Franklin Nwanze <nwanzechuka1993@gmail.com>
2017-10-11 14:47:11 +0100

    autodeploy

--- deploy.php
+++ deploy.php
@@ -10,7 +10,7 @@
     $commands = array(
         'echo $PWD',
         'whoami',
-        'git pull',
+        'git pull origin master',
         'git status',
         'git submodule sync',
         'git submodule update',
